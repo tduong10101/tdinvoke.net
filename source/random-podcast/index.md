e47a27605d30aac6462c35e0a8c4f6fe931abff1
Terry Duong <tduong10101@gmail.com>
2019-09-22 23:37:58 +1000

    add random podcast page and scripts

---
+++ source/random-podcast/index.md
@@ -0,0 +1,5 @@
+---
+title: Random Podcast
+date: 2019-09-22 17:00:00
+---
+<div id="current-podcast"><audio id='audio' style="display:none" controls><source id='audio-source' type='audio/mpeg'></audio></div>
---
+++ themes/chan/source/podcast/podcast.js
@@ -0,0 +1,35 @@
+var API_GET_CURRENT_EP = "https://pw9hp8mso2.execute-api.us-east-1.amazonaws.com/default/td-podcast-get-current-episode"
+$(document).ready(function() {
+            $.ajax({
+                    url: API_GET_CURRENT_EP,
+                    type: 'GET',
+                    success: function(response) {
+                        // $("#title").append(response.title)
+                        // $("#station").append(response.station)
+                        var intro = "Welcome to my random podcast page, each time an episode is finished, another random episode will be replaced (after refresh) by my td-podcast serverless app. For more information about the app please see this post. <br><br>Current episode is <strong>"+(response.title)+"</strong> from <strong>"+(response.station)+"</strong>.<br><br>"
+                        $("#current-podcast").prepend(intro)
+                        $("#current-podcast").append("<br><p><strong>Description:</strong> "+response.description+"</p>")
+                        $("#audio-source").attr('src',response.episode_url);
+                        $("#audio").attr('style','display:block');
+                        $("#audio").trigger('load')
+                            },
+                            error: function() {
+                                alert("error");
+                            }
+                    });
+            });
+var aud = document.getElementById("audio");
+var API_GEN_RAN_EP = "https://pw9hp8mso2.execute-api.us-east-1.amazonaws.com/default/td-podcast-gen-random-episode"
+aud.onended = function() {
+    $.ajax({
+            url: API_GEN_RAN_EP,
+            type: 'GET',
+            success: function(response) {
+                console("gen_ran_ep")
+            },
+            error: function() {
+                        alert("error");
+                    }
+            });
+            window.setTimeout(function(){location.reload();}, 5000);
+    };
